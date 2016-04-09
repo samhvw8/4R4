dddc57f75dd19c035fc625119e700d152d10ddbd
Sam Hoang <samhv.ict@gmail.com>
2016-04-09 19:34:07 +0700

    make street, district, ward, city cols in rooms table is unique

--- database/migrations/2016_03_05_045006_create_rooms_table.php
+++ database/migrations/2016_03_05_045006_create_rooms_table.php
@@ -19,13 +19,14 @@ class CreateRoomsTable extends Migration
             $table->double('area');
             $table->string('decripstion')->nullable(true);
             $table->string('image_album_url');
-            $table->double('latitude');
-            $table->double('longitude');
+            $table->double('latitude')->nullable(true);
+            $table->double('longitude')->nullable(true);
             $table->string('street');
             $table->string('district');
             $table->string('ward');
             $table->string('city');
-            
+
+            $table->unique(array('street', 'district', 'ward', 'city'));
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
