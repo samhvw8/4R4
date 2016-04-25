614fe5a171ff9368a0f7576fc41eba29aa3494ef
Sam Hoang <samhv.ict@gmail.com>
2016-04-25 11:37:56 +0700

    Make some API can use by guest user

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,30 +30,116 @@ Route::group(['middleware' => ['web']], function () {
     //
 });
 
+Route::group(['prefix' => 'api/v1'], function () {
 
-Route::group([
-    'prefix' => 'api/v1',
-    'middleware' => 'auth.basic'
-], function () {
-    //    ====================== User ================================
 
-    /**
-     * Get all users
-     */
+    Route::group(['middleware' => 'auth.basic'], function () {
+        //    ====================== User ================================
 
-    Route::get('users', [
-        'as' => 'user.all',
-        'uses' => 'Api\v1\UsersController@all'
-    ]);
+        /**
+         * Get all users
+         */
 
-    /**
-     * Create a new user
-     */
-    Route::post('user', [
-        'as' => 'user.create',
-        'uses' => 'Api\v1\UsersController@create'
-    ]);
+        Route::get('users', [
+            'as' => 'user.all',
+            'uses' => 'Api\v1\UsersController@all'
+        ]);
 
+        /**
+         * Create a new user
+         */
+        Route::post('user', [
+            'as' => 'user.create',
+            'uses' => 'Api\v1\UsersController@create'
+        ]);
+
+        /**
+         * Update user info
+         */
+        Route::put('user/{id}', [
+            'as' => 'user.update',
+            'uses' => 'Api\v1\UsersController@update'
+        ]);
+
+        /**
+         * Delete user
+         */
+        Route::delete('user/{id}', [
+            'as' => 'user.delete',
+            'uses' => 'Api\v1\UsersController@delete'
+        ]);
+
+        /**
+         * Get user's rooms by id
+         */
+
+        Route::get('user/{id}/rooms', [
+            'as' => 'user.getRoom',
+            'uses' => 'Api\v1\UsersController@getRoom'
+        ]);
+
+
+        Route::group(['middleware' => 'r4r\Http\Middleware\AdminCheck'], function () {
+            /**
+             * Get all admins
+             */
+
+            Route::get('admins', [
+                'as' => 'user.allAdmin',
+                'uses' => 'Api\v1\UsersController@allAdmin'
+            ]);
+            /**
+             * Make user admin
+             */
+
+            Route::get('user/{id}/admin', [
+                'as' => 'user.makeAdmin',
+                'uses' => 'Api\v1\UsersController@makeAdmin'
+            ]);
+
+            /**
+             * Delete admin
+             */
+            Route::get('user/{id}/deladmin', [
+                'as' => 'user.delAdmin',
+                'uses' => 'Api\v1\UsersController@delAdmin'
+            ]);
+
+
+            Route::get('room_update_addess', [
+                'as' => 'room.update_address',
+                'uses' => 'Api\v1\RoomsController@updateAdress'
+            ]);
+
+        });
+
+//    ====================== Room ================================
+
+        /**
+         * Create a new room
+         */
+        Route::post('room', [
+            'as' => 'room.create',
+            'uses' => 'Api\v1\RoomsController@create'
+        ]);
+
+        /**
+         * Update room  info
+         */
+        Route::put('room/{id}', [
+            'as' => 'room.update',
+            'uses' => 'Api\v1\RoomsController@update'
+        ]);
+
+        /**
+         * Delete room
+         */
+        Route::delete('room/{id}', [
+            'as' => 'room.delete',
+            'uses' => 'Api\v1\RoomsController@delete'
+        ]);
+
+    });
 
     /**
      * Get user info by id
@@ -63,60 +149,6 @@ Route::group([
         'uses' => 'Api\v1\UsersController@get'
     ]);
 
-    /**
-     * Update user info
-     */
-    Route::put('user/{id}', [
-        'as' => 'user.update',
-        'uses' => 'Api\v1\UsersController@update'
-    ]);
-
-    /**
-     * Delete user
-     */
-    Route::delete('user/{id}', [
-        'as' => 'user.delete',
-        'uses' => 'Api\v1\UsersController@delete'
-    ]);
-
-    /**
-     * Get user's rooms by id
-     */
-
-    Route::get('user/{id}/rooms', [
-        'as' => 'user.getRoom',
-        'uses' => 'Api\v1\UsersController@getRoom'
-    ]);
-
-
-    Route::group(['middleware' => 'r4r\Http\Middleware\AdminCheck'], function () {
-        /**
-         * Get all admins
-         */
-
-        Route::get('admins', [
-            'as' => 'user.allAdmin',
-            'uses' => 'Api\v1\UsersController@allAdmin'
-        ]);
-        /**
-         * Make user admin
-         */
-
-        Route::get('user/{id}/admin', [
-            'as' => 'user.makeAdmin',
-            'uses' => 'Api\v1\UsersController@makeAdmin'
-        ]);
-
-        /**
-         * Delete admin
-         */
-        Route::get('user/{id}/deladmin', [
-            'as' => 'user.delAdmin',
-            'uses' => 'Api\v1\UsersController@delAdmin'
-        ]);
-    });
-
-//    ====================== Room ================================
 
     /**
      * Get all rooms
@@ -126,14 +158,6 @@ Route::group([
         'uses' => 'Api\v1\RoomsController@all'
     ]);
 
-    /**
-     * Create a new room
-     */
-    Route::post('room', [
-        'as' => 'room.create',
-        'uses' => 'Api\v1\RoomsController@create'
-    ]);
-
 
     /**
      * Get room info by id
@@ -141,28 +165,6 @@ Route::group([
     Route::get('room/{id}', [
         'as' => 'room.get',
         'uses' => 'Api\v1\RoomsController@get'
-    ]);
-
-    /**
-     * Update room  info
-     */
-    Route::put('room/{id}', [
-        'as' => 'room.update',
-        'uses' => 'Api\v1\RoomsController@update'
-    ]);
-
-    /**
-     * Delete room
-     */
-    Route::delete('room/{id}', [
-        'as' => 'room.delete',
-        'uses' => 'Api\v1\RoomsController@delete'
-    ]);
-
-
-    Route::get('room_update_addess', [
-        'as' => 'room.update_address',
-        'uses' => 'Api\v1\RoomsController@updateAdress'
     ]);
 
     /**
@@ -185,4 +187,7 @@ Route::group([
             'uses' => 'Api\v1\RoomsController@searchAddress'
         ]);
     });
+
+
 });
+
